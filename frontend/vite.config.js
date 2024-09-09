3a401d5053676b6f9fb200c7f7e8db812fb4dba0
rakesh <rakeshmanna762@gmail,com>
2024-09-09 23:12:54 +0530

    vite config changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'https://movies-hub-kohl.vercel.app',
+        target:'http://localhost:5000',
         
       }
     }
