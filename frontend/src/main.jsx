6efd84a0bbe2a7ea561731d7a04062384ef38aad
rakesh <rakeshmanna762@gmail,com>
2024-09-09 23:24:17 +0530

    HashRouter added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,16 +3,16 @@ import { createRoot } from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-    <BrowserRouter>
+  <StrictMode>
+    <HashRouter>
       <ChakraProvider>
 
         <App />
       </ChakraProvider>
-    </BrowserRouter>
-  /* </StrictMode>, */
+    </HashRouter>
+  </StrictMode>
 )
